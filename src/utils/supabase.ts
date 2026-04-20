e19cd39d36f0b36c3f9b4071fccd88461f349e3d
Kenzo <90186133@bri.co.id>
2026-04-20 11:08:42 +0700

    feat: tambah fitur login

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,10 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseUrl = 'https://kkancpslpmlbmgjczyaj.supabase.co';
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Missing Supabase environment variables. Please set VITE_SUPABASE_URL and VITE_SUPABASE_PUBLISHABLE_KEY.')
+  throw new Error('Missing Supabase publishable key. Please set VITE_SUPABASE_PUBLISHABLE_KEY.')
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
+  },
+});
